f15017350c8bf0664b36a923a207dd87367e812f
Aaron <33428648+AaronVick@users.noreply.github.com>
2024-08-21 08:14:14 -0500

    update

--- pages/api/historyFrame.js
+++ pages/api/historyFrame.js
@@ -43,8 +43,8 @@ async function handleHistoryNavigation(res, direction) {
         <meta property="fc:frame:image" content="${ogImageUrl}" />
         <meta property="fc:frame:button:1" content="Previous" />
         <meta property="fc:frame:button:2" content="Next" />
-        <meta property="fc:frame:button:3" content="Share" />
-        <meta property="fc:frame:post_url" content="https://time-capsule-jade.vercel.app/api/historyFrame" />
+        <meta property="fc:frame:button:3:action" content="link" />
+        <meta property="fc:frame:button:3:target" content="https://warpcast.com/~/compose?text=Check+out+today's+moments+in+history!%0A%0AFrame+by+%40aaronv&embeds[]=https%3A%2F%2Ftime-capsule-jade.vercel.app" />
       </head>
     </html>
   `);
